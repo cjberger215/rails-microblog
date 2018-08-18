a58b9c2cc24eb00a6a6ad770ac912864ecb5d8aa
Cliff Berger <cjberger215@gmail.com>
2018-08-18 13:03:21 -0700

    add admin property to users and test that cannot be edited via update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,7 +6,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
   end
@@ -27,6 +27,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password:              'password',
+              password_confirmation: 'password',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
   end
 
   test 'should redirect edit when logged in as wrong user' do
