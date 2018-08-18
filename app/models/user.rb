fe7357e4aca15f4441c7321c9dd753146e04b829
Cliff Berger <cjberger215@gmail.com>
2018-08-18 15:31:42 -0700

    restrict show and index to activated users"

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-  
+
   # Returns true if the given token matches the digest.
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
@@ -41,11 +41,21 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account.
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   # Converts email to all lower-case.
   def downcase_email
-    self.email.downcase!
+    email.downcase!
   end
 
   # Creates and assigns the activation token and digest.
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @inactive_user = users(:lana)
   end
 
   test 'should redirect index when not logged in' do
@@ -67,6 +68,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       delete user_path(@user)
     end
+    assert_redirected_to root_url
+  end
+  
+  test 'should redirect show when user not activated' do
+    get user_path(@inactive_user)
     assert_redirected_to root_url
   end
 end
