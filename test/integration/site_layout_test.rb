61c38271edc537a150d46bed27636fb566fe6e63
Cliff Berger <cjberger215@gmail.com>
2018-08-18 12:26:25 -0700

    add layout links tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  test 'layout links for unauthenticated users' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+
+  test 'layout links for authenticated users' do
+    user = users(:michael)
+    log_in_as user
+    get root_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', "/users/#{user.id}"
+    assert_select 'a[href=?]', "/users/#{user.id}/edit"
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
   end
